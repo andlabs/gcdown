3684692af9f6695d752d64eb3d45e7f387524669
Pietro Gagliardi <pietro10@mac.com>
2015-03-27 14:40:27 -0400

    More collector work.

--- main.go
+++ main.go
@@ -5,23 +5,29 @@ import (
 	"fmt"
 //	"os"
 	"net/http"
+	"path/filepath"
 	"golang.org/x/net/html"
 )
 
 const URL = "https://code.google.com/p/bsnes/downloads/list?can=1&q=&colspec=Filename+Summary+Uploaded+ReleaseDate+Size+DownloadCount"
 
-func main() {
+type Download struct {
+	URL		string
+	Filename	string
+}
+
+func collect(url string) (downloads []*Download, err error) {
 	var f func(*html.Node)
 
 	resp, err := http.Get(URL)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	f = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "a" {
@@ -36,7 +42,10 @@ func main() {
 				}
 			}
 			if is {
-				fmt.Println(href)
+				downloads = append(downloads, &Download{
+					URL:			"https:" + href,
+					Filename:		filepath.Base(href),
+				})
 			}
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
@@ -44,4 +53,10 @@ func main() {
 		}
 	}
 	f(doc)
+
+	return downloads, nil
+}
+
+func main() {
+	fmt.Println(collect(URL))
 }
